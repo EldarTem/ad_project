e5c8b536e37dbbce7ae1f87e6f4358c3a611f5fc
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-03-30 22:06:23 +0300

    Подключение экранов к роутеру

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,6 +18,7 @@
   <v-list-item
   v-for="link in links"
   :key="link.title"
+  :to="link.url"
   >
   <template v-slot:prepend>
   <v-icon :icon="link.icon"></v-icon>
@@ -34,6 +35,7 @@
   text
   v-for="link in links"
   :key="link.title"
+  :to="link.url"
   >
   <v-icon
   start
