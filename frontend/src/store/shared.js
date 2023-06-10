c40216fa793b531d56af9f0778210bf6098839f7
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-06-10 20:47:24 +0300

    Подключение лоудера на экране регистрации

--- frontend/src/store/shared.js
+++ frontend/src/store/shared.js
@@ -15,7 +15,6 @@ export default {
                state.error = null
           }
      },
-     
      actions: {
           setLoading ({commit}, payload) {
                commit('setLoading', payload)
