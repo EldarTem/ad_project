d24dc6642552a90f368874f5c050c3a3b79f9a86
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-06-16 20:24:11 +0300

    Управление кнопкой Edit

--- frontend/src/views/Ads/AdView.vue
+++ frontend/src/views/Ads/AdView.vue
@@ -12,7 +12,7 @@
                   </v-card-text>
                   <v-card-actions>
                       <v-spacer></v-spacer>
-                      <modal-dialog :ad="ad"></modal-dialog>
+                      <modal-dialog :ad="ad" v-if="isOwner"></modal-dialog>
                       <v-btn class="success" color="green">Buy</v-btn>
                   </v-card-actions>
               </v-card>
@@ -32,6 +32,9 @@ export default {
     },
     components: {
 		'modal-dialog': EditAdModal
+    },
+    isOwner () {
+	return this.ad.ownerId === 	this.$store.getters.user.id
     }
 }
 </script>
