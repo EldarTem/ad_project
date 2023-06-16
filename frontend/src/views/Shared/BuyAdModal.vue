7a3b1064d80f9cf200451477935cfe0fdc32cca4
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-06-16 20:49:32 +0300

    Создание и подключение компонента BuyAdModel

--- frontend/src/views/Shared/BuyAdModal.vue
+++ frontend/src/views/Shared/BuyAdModal.vue
@@ -11,30 +11,16 @@
     <v-row justify="center">
     <v-col cols="12">
     <v-card-title>
-    <h1 class="text--primary">Do you wan't to buy it?</h1>
+    <h1 class="text--primary">Buy It</h1>
     </v-card-title>
     </v-col>
     </v-row>
     
     <v-row justify="center">
     <v-col cols="12">
-        <v-card-text>
-            <v-text-field
-            name="name"
-            label="Your name"
-            type="text"
-            v-model="name"
-            >
-            </v-text-field>
-            <v-text-field
-            name="phone"
-            label="Your phone"
-            type="text"
-            v-model="phone"
-            >
-            </v-text-field>
-        </v-card-text>
-
+    <v-card-text>
+    Lorem ipsum.
+    </v-card-text>
     </v-col>
     </v-row>
     
@@ -56,10 +42,7 @@ export default {
 props: ['ad'],
 data() {
     return {
-    modal: false,
-    name: '',
-    phone: ''
-
+    modal: false
     }
 }
 }
